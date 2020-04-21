a4a8d5b4f6355a339b91a8c2af149e5edfbe060f
William John Holden <wjholden@gmail.com>
2020-04-21 07:48:20 +0200

    Switch to List interface instead of basic arrays and parameterized types.

--- src/com/wjholden/Main.java
+++ src/com/wjholden/Main.java
@@ -1,24 +1,31 @@
 package com.wjholden;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 
 public class Main {
 
-    private final static List<String> db = new ArrayList<String>();
+    private final static List<String> db = new ArrayList<>();
 
     private final static Page database = new Page("Database", "Database",
-            new Page[0], db::toArray);
+            new Page[0],
+            () -> db);
     private final static Page doubles = new Page("Doubles (Random 1000)", "Doubles",
-            new Page[] { database }, () -> DoubleStream.generate(Math::random).limit(1000).boxed().toArray());
+            new Page[] { database },
+            () -> DoubleStream.generate(Math::random).limit(1000).boxed().map(Object::toString).collect(Collectors.toList()));
     private final static Page integers = new Page("Integers (Random 100)", "Integers",
-            new Page[] { doubles, database }, () -> IntStream.generate(() -> (int) (1000 * Math.random())).limit(100).boxed().toArray());
+            new Page[] { doubles, database },
+            () -> IntStream.generate(() -> (int) (1000 * Math.random())).limit(100).boxed().map(Object::toString).collect(Collectors.toList()));
     private final static Page empty = new Page("Empty", "Empty",
-            new Page[0], () -> new String[0]);
-    private final static Page mainPage = new Page("Main Menu", null,
-            new Page[] { doubles, integers, database, empty }, () -> new String[] { "Hello", "World"});
+            new Page[0],
+            Collections::emptyList);
+    private final static Page mainPage = new Page("Main Menu", "Main",
+            new Page[] { doubles, integers, database, empty },
+            () -> List.of("Hello", "World"));
 
     public static void main(String[] args) {
         new Thread(new Syslog(514, db)).start();
--- src/com/wjholden/Page.java
+++ src/com/wjholden/Page.java
@@ -1,19 +1,20 @@
 package com.wjholden;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 public class Page {
     public final String name, description;
-    private final Supplier<Object[]> reportFunction;
+    private final Supplier<List<String>> reportFunction;
     private int offset = 0;
     private int more = 0;
     private static final String title = "Page.java";
     private final Page[] children;
     private static final int WIDTH = 80;
     private static final boolean showColumnNumbers = false;
-    private Object[] report;
+    private List<String> report;
 
-    public Page(String name, String description, Page[] children, Supplier<Object[]> reportFunction) {
+    public Page(String name, String description, Page[] children, Supplier<List<String>> reportFunction) {
         this.reportFunction = reportFunction;
         this.name = name;
         this.description = description;
@@ -27,53 +28,53 @@ public class Page {
 
     @Override
     public String toString() {
-        String str = "";
+        StringBuilder str = new StringBuilder();
 
         if (showColumnNumbers) {
             for (int i = 1; i <= 80; i++) {
-                str += i >= 10 ? i / 10 : " ";
+                str.append(i >= 10 ? i / 10 : " ");
             }
-            str += "\r\n";
+            str.append("\r\n");
             for (int i = 1; i <= 80; i++) {
-                str += (i % 10);
+                str.append(i % 10);
             }
-            str += "\r\n";
+            str.append("\r\n");
         }
 
-        str += center(title, WIDTH).toUpperCase() + "\r\n";
-        str += center(name, WIDTH).toUpperCase() + "\r\n";
+        str.append(center(title, WIDTH).toUpperCase()).append("\r\n");
+        str.append(center(name, WIDTH).toUpperCase()).append("\r\n");
         for (int i = 0 ; i < 15 ; i++) {
             final int position = offset + i;
-            final int number_width = 1 + (report.length > 0 ? (int) Math.log10(report.length) : 0);
-            str += String.format("%" + number_width + "d: ", position + 1);
-            if (position < report.length) {
-                str += String.format("%-" + (WIDTH - 3 - number_width) + "s", report[position]);
+            final int number_width = 1 + (report.isEmpty() ? 0 : (int) Math.log10(report.size()));
+            str.append(String.format("%" + number_width + "d: ", position + 1));
+            if (position < report.size()) {
+                str.append(String.format("%-" + (WIDTH - 3 - number_width) + "s", report.get(position)));
             }
-            str += "\r\n";
+            str.append("\r\n");
         }
 
-        str += "\r\n";
+        str.append("\r\n");
 
         for (int i = 1 ; i <= 4 ; i++) {
             if (children.length >= i + more) {
-                str += i + "-" + leftAlignFixed(children[more+i-1].description, 12).toUpperCase();
+                str.append(i).append("-").append(leftAlignFixed(children[more + i - 1].description, 12).toUpperCase());
             } else {
-                str += i + "-" + leftAlignFixed("", 12);
+                str.append(i).append("-").append(leftAlignFixed("", 12));
             }
         }
 
-        str += "5-";
+        str.append("5-");
         if (children.length > 5) {
-            str += "MORE";
+            str.append("MORE");
         } else if (children.length == 5) {
-            str += leftAlignFixed(children[4].description, 12).toUpperCase();
+            str.append(leftAlignFixed(children[4].description, 12).toUpperCase());
         }
-        str += "\r\n";
+        str.append("\r\n");
 
         // common parameters to all pages
-        str += "6-REFRESH     7-PAGE_UP     8-PAGE_DOWN   9-BACK        0-EXIT        ";
+        str.append("6-REFRESH     7-PAGE_UP     8-PAGE_DOWN   9-BACK        0-EXIT        ");
 
-        return str;
+        return str.toString();
     }
 
     public Page command(char c) throws InterruptedException {
@@ -101,13 +102,13 @@ public class Page {
         return this;
     }
 
-    private static final String leftAlignFixed(String string, int width) {
+    private static String leftAlignFixed(String string, int width) {
         assert(width > 0);
 
         return String.format("%-" + width + "." + width + "s", string);
     }
 
-    private static final String center(String string, int width) {
+    private static String center(String string, int width) {
         assert(width > 0);
 
         return String.format("%" + (string.length() + (width - string.length())/2) + "s", string);
